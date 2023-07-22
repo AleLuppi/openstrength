eafeffaf44c5a81f8241cbff680af023e36e1e0f
Lorenzo <lorenzobof@outlook.it>
2023-07-22 11:41:25 +0200

    added multiple chart components for test purposes

--- frontend/src/views/DashboardView.vue
+++ frontend/src/views/DashboardView.vue
@@ -2,9 +2,25 @@
   <div class="dashboard">
     <chart-component
       title="Workout Volume"
-      description="Volume of workouts over time"
+      description="Volume of workouts over time computed as Rep x Set x Load"
       :data="chartData"
       :options="chartOptions"
+      :width="400"
+      :height="200"
+    />
+    <chart-component
+      title="Volume alzate principali"
+      description="Just another graph to test multiple components"
+      :data="chartData2"
+      :options="chartOptions2"
+      :width="400"
+      :height="200"
+    />
+    <chart-component
+      title="1RM alzate principali"
+      description="Just another graph to test multiple components"
+      :data="chartData1RM"
+      :options="chartOptions1RM"
       :width="400"
       :height="200"
     />
@@ -68,6 +84,110 @@ export default {
             title: {
               display: true,
               text: 'Volume',
+            },
+          },
+        },
+      },
+      chartData2: {
+        datasets: [
+          {
+            label: 'Squat',
+            data: [200, 600, 1000, 1600, 2000, 2300, 1600, 1000, 600],
+            borderColor: 'rgba(0, 123, 255, 1)',
+            backgroundColor: 'rgba(0, 123, 255, 0.2)',
+            fill: false,
+            cubicInterpolationMode: 'monotone',
+            tension: 0.1,
+          },
+          {
+            label: 'Panca',
+            data: [1600, 1400, 1300, 1000, 1400, 1600, 1900, 2000, 2400],
+            borderColor: 'red',
+            backgroundColor: 'rgba(255, 0, 0, 0.2)',
+            fill: false,
+            cubicInterpolationMode: 'monotone',
+            tension: 0.1,
+          },
+          {
+            label: 'Stacco',
+            data: [1500, 1600, 2000, 1000, 500, 800, 1800, 2000, 2500],
+            borderColor: 'green',
+            backgroundColor: 'rgba(255, 0, 0, 0.2)',
+            fill: false,
+            cubicInterpolationMode: 'monotone',
+            tension: 0.1,
+          },
+        ],
+        labels: ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre'],
+      },
+      chartOptions2: {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          x: {
+            display: true,
+            title: {
+              display: true,
+              text: 'Mese',
+            },
+          },
+          y: {
+            display: true,
+            title: {
+              display: true,
+              text: 'Volume',
+            },
+          },
+        },
+      },
+      chartData1RM: {
+        datasets: [
+          {
+            label: 'Squat',
+            data: [180, 185, 192.5, 200, 205, 215],
+            borderColor: 'rgba(0, 123, 255, 1)',
+            backgroundColor: 'rgba(0, 123, 255, 0.2)',
+            fill: false,
+            cubicInterpolationMode: 'monotone',
+            tension: 0.1,
+          },
+          {
+            label: 'Panca',
+            data: [105, 107.5, 112.5, 120, 120, 122.5],
+            borderColor: 'red',
+            backgroundColor: 'rgba(255, 0, 0, 0.2)',
+            fill: false,
+            cubicInterpolationMode: 'monotone',
+            tension: 0.1,
+          },
+          {
+            label: 'Stacco',
+            data: [190, 205, 210, 210, 215, 220],
+            borderColor: 'green',
+            backgroundColor: 'rgba(255, 0, 0, 0.2)',
+            fill: false,
+            cubicInterpolationMode: 'monotone',
+            tension: 0.1,
+          },
+        ],
+        labels: ['Gennaio', 'Marzo', 'Maggio', 'Luglio', 'Settembre', 'Dicembre'],
+      },
+      chartOptions1RM: {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          x: {
+            display: true,
+            title: {
+              display: true,
+              text: 'Mese',
+            },
+          },
+          y: {
+            display: true,
+            title: {
+              display: true,
+              text: '1RM [kg]',
             },
           },
         },
