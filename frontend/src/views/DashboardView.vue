1ff5001b8f1c47df973f13bf7317f09da06abbc1
Lorenzo <lorenzobof@outlook.it>
2023-07-24 21:25:09 +0200

    Modified chart and solved data binding, still missing correct labeling

--- frontend/src/views/DashboardView.vue
+++ frontend/src/views/DashboardView.vue
@@ -2,7 +2,7 @@
   <div class="dashboard">
     <chart-component title="Workout Volume" description="Volume of workouts over time computed as Rep x Set x Load"
       :data="chartData" :options="chartOptions" :width="400" :height="300" />
-    <chart-component title="Volume alzate principali" description="Just another graph to test multiple components"
+   <!--  <chart-component title="Volume alzate principali" description="Just another graph to test multiple components"
       :data="chartData2" :options="chartOptions2" :width="400" :height="300" />
     <chart-component title="1RM alzate principali" description="Just another graph to test multiple components"
       :data="chartData1RM" :options="chartOptions1RM" :width="400" :height="300" />
@@ -15,7 +15,7 @@
     :width="400"
     :height="300"
     />
-
+ -->
     <bar-line-chart-component
       title="Progressi Mesociclo"
       description="Questo grafico mostra l'andamento di serie e rep all'interno di un mesociclo per un esercizio."
@@ -34,8 +34,10 @@
 <script>
 import ChartComponent from '@/components/charts/ChartComponent.vue';
 import chartData from '@/views/chartdata/chartData.json';
-import chartOptions from '@/views/chartdata/chartOptions.json';
-import chartData2 from '@/views/chartdata/chartData2.json';
+
+console.log("chartdata here it is");
+console.log(chartData);
+/* import chartData2 from '@/views/chartdata/chartData2.json';
 import chartOptions2 from '@/views/chartdata/chartOptions2.json';
 import chartData1RM from '@/views/chartdata/chartData1RM.json';
 import chartOptions1RM from '@/views/chartdata/chartOptions1RM.json'; 
@@ -45,7 +47,9 @@ import barLineChartData from '@/views/chartdata/barLineChartData.json';
 import barLineChartOptions from '@/views/chartdata/barLineChartOptions.json';
 import barChartData from '@/views/chartdata/barChartData.json';
 import barChartOptions from '@/views/chartdata/barChartOptions.json';
+ */
 
+import chartOptions from '@/views/chartdata/chartOptions.json';
 import CompleteChartComponent from '@/components/charts/CompleteChartComponent.vue';
 import data from '@/views/data/datapreprocessing.js';
 
@@ -55,20 +59,20 @@ export default {
   components: {
     ChartComponent,
     CompleteChartComponent,
-    BarLineChartComponent,
+   // BarLineChartComponent,
   },
   data() {
     return {
       chartData,
       chartOptions,
-      chartData2,
+     /*  chartData2,
       chartOptions2,
       chartData1RM,
       chartOptions1RM,
       barLineChartData,
       barLineChartOptions,
       barChartData,
-      barChartOptions,
+      barChartOptions, */
       data,
     };
   },
--- frontend/src/views/data/datapreprocessing.js
+++ frontend/src/views/data/datapreprocessing.js
@@ -12,24 +12,48 @@ function computeVolume(exercise) {
   const reps = parseJSON(exercise.reps);
   const load = parseJSON(exercise.load);
 
-  const volume = reps.map((rep, index) => rep * load[index]);
+  if (reps.length !== load.length) {
+    const errorMessage = "The lengths of reps and load arrays must be equal! Check your database.";
+    alert(errorMessage);
+    throw new Error(errorMessage);
+  }
+
+  let volume = 0;
+  for (let i = 0; i < reps.length; i++) {
+    volume += reps[i] * load[i];
+  }
+
   return volume;
 }
 
+
 // Loop through the assignedFitness dataInput and compute the volume for each exercise
 const assignedFitness = dataInput.assignedFitness;
-const data = [];
+const data = {
+  datasets: [],
+  labels: [],
+};
 
 assignedFitness.forEach((entry) => {
   const workoutEntries = entry.mesocycleEntry.flatMap((mesocycle) => mesocycle.weekEntry.flatMap((week) => week.workoutEntry));
-  
+  const weekNameEntry = entry.mesocycleEntry.flatMap((mesocycle) => mesocycle.weekEntry.flatMap((week) => week.weekName));
+
   workoutEntries.forEach((workout) => {
     const exercises = workout.exercise;
-    exercises.forEach((exercise) => {
-      const volume = computeVolume(exercise);
-      data.push({ name: exercise.name, volume });
-    });
+    const volumeData = exercises.map((exercise) => computeVolume(exercise));
+    
+    data.datasets.push({
+      label: exercises.name,
+      data: volumeData,
+    }
+    );
   });
+
+  if (workoutEntries.length > 0) {
+    data.labels.push(...weekNameEntry); //Spread the elements of weekNameEntry into the labels array
+  }
+
 });
 
+console.log(data);
 export default data;
