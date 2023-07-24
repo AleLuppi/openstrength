b69c48ca80e604c5a5213b329fb3fa47ef0efb3a
Lorenzo <lorenzobof@outlook.it>
2023-07-24 22:55:09 +0200

    Modified database JSON adding other fields

--- frontend/src/views/DashboardView.vue
+++ frontend/src/views/DashboardView.vue
@@ -2,7 +2,7 @@
   <div class="dashboard">
     <chart-component title="Workout Volume" description="Volume of workouts over time computed as Rep x Set x Load"
       :data="chartData" :options="chartOptions" :width="400" :height="300" />
-   <!--  <chart-component title="Volume alzate principali" description="Just another graph to test multiple components"
+    <chart-component title="Volume alzate principali" description="Just another graph to test multiple components"
       :data="chartData2" :options="chartOptions2" :width="400" :height="300" />
     <chart-component title="1RM alzate principali" description="Just another graph to test multiple components"
       :data="chartData1RM" :options="chartOptions1RM" :width="400" :height="300" />
@@ -13,16 +13,16 @@
     :data="barLineChartData" 
     :options="barLineChartOptions" 
     :width="400"
-    :height="300"
+    :height="150"
     />
- -->
+
     <bar-line-chart-component
       title="Progressi Mesociclo"
       description="Questo grafico mostra l'andamento di serie e rep all'interno di un mesociclo per un esercizio."
       :data="barChartData" 
       :options="barChartOptions" 
       :width="400"
-      :height="300"
+      :height="150"
     />
 
     <complete-chart-component title="Example preprocessing" description="Volume data taken from complete JSON for athlete"
@@ -37,7 +37,7 @@ import chartData from '@/views/chartdata/chartData.json';
 
 console.log("chartdata here it is");
 console.log(chartData);
-/* import chartData2 from '@/views/chartdata/chartData2.json';
+import chartData2 from '@/views/chartdata/chartData2.json';
 import chartOptions2 from '@/views/chartdata/chartOptions2.json';
 import chartData1RM from '@/views/chartdata/chartData1RM.json';
 import chartOptions1RM from '@/views/chartdata/chartOptions1RM.json'; 
@@ -47,7 +47,7 @@ import barLineChartData from '@/views/chartdata/barLineChartData.json';
 import barLineChartOptions from '@/views/chartdata/barLineChartOptions.json';
 import barChartData from '@/views/chartdata/barChartData.json';
 import barChartOptions from '@/views/chartdata/barChartOptions.json';
- */
+
 
 import chartOptions from '@/views/chartdata/chartOptions.json';
 import CompleteChartComponent from '@/components/charts/CompleteChartComponent.vue';
@@ -59,20 +59,20 @@ export default {
   components: {
     ChartComponent,
     CompleteChartComponent,
-   // BarLineChartComponent,
+    BarLineChartComponent,
   },
   data() {
     return {
       chartData,
       chartOptions,
-     /*  chartData2,
+      chartData2,
       chartOptions2,
       chartData1RM,
       chartOptions1RM,
       barLineChartData,
       barLineChartOptions,
       barChartData,
-      barChartOptions, */
+      barChartOptions,
       data,
     };
   },
