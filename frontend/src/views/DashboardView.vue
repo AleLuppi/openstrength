67a4964f4bafd842a3c3e26007a2cff0bfd1c8c1
Lorenzo <lorenzobof@outlook.it>
2023-07-29 12:01:24 +0200

    Modified graph F Reorganized values and in general changed column width for existing charts

--- frontend/src/views/DashboardView.vue
+++ frontend/src/views/DashboardView.vue
@@ -3,42 +3,42 @@
     <!-- TODO dynamic sizing, and auto add title + description -->
 
     <!-- A: andamento Volume, Intensita, IRT per esercizio -->
-    <div class="col-12">
+    <div class="col-4">
       <chart-component title="A: Panca - Andamento parametri nel mesociclo" description="Andamento dell'intensità, volume e IRT nel mesociclo di riferimento."
         :data="dataA" :options="optionA" :width="400" :height="300" />
     </div>
 
-    <div class="col-12">
+    <div class="col-4">
       <chart-component title="A2: Stacco - Andamento parametri nel mesociclo" description="Andamento dell'intensità, volume e IRT nel mesociclo di riferimento."
         :data="dataA2" :options="optionA" :width="400" :height="300" />
     </div>
 
-    <div class="col-12">
+    <div class="col-4">
       <chart-component title="A3: Squat - Andamento parametri nel mesociclo" description="Andamento dell'intensità, volume e IRT nel mesociclo di riferimento."
         :data="dataA3" :options="optionA" :width="400" :height="300" />
     </div>
 
     <!-- B: andamento Volume x Intensita per ogni seduta-->
-      <div class="col-12">
+      <div class="col-6">
       <chart-component title="B: Andamento Carico di allenamento" description="Andamento dell'intensità x volume nel corso del mesociclo per seduta."
         :data="dataB" :options="optionB" :width="400" :height="300" />
       </div>
 
     <!-- C: andamento Serie x Intensita Relativa per ogni seduta-->
-      <div class="col-12">
+      <div class="col-6">
       <chart-component title="C: Andamento Carico di allenamento" description="Andamento dell'intensità relativa x volume nel corso del mesociclo per seduta."
         :data="dataC" :options="optionC" :width="400" :height="300" />
       </div>
 
     <!-- D: Andamento del Volume x Intensità nelle settimane per i singoli esercizi -->
-    <div class="col-12">
+    <div class="col-6">
       <chart-component title="D: Andamento Carico di allenamento per esercizio" type="bar"
         description="Questo grafico mostra l'andamento del Volume x Intensità per singolo esercizio"
         :data="dataD" :options="optionD" :width="400" :height="150" />
     </div>
 
     <!-- D2: Uguale al precedente ma sottoforma di line graph -->
-    <div class="col-12">
+    <div class="col-6">
       <chart-component title="D2: Andamento Carico di allenamento per esercizio" type="bar"
         description="Questo grafico mostra l'andamento del Volume x Intensità per singolo esercizio"
         :data="dataD2" :options="optionD2" :width="400" :height="150" />
@@ -50,7 +50,11 @@
        <v-grid-component />
     </div>
 
-
+    <!-- F: 1RM alzate principali vs peso corporeo -->
+    <div class="col-5">
+      <chart-component title="1RM alzate principali" description="Andamento dell'1RM versus peso corporeo."
+        :data="dataF" :options="optionF" :width="400" :height="300" />
+    </div>
 
 
     <div class="col-12">
@@ -61,10 +65,7 @@
       <chart-component title="Volume alzate principali" description="Just another graph to test multiple components"
         :data="chartData2" :options="chartOptions2" :width="400" :height="300" />
     </div>
-    <div class="col-5">
-      <chart-component title="1RM alzate principali" description="Just another graph to test multiple components"
-        :data="chartData1RM" :options="chartOptions1RM" :width="400" :height="300" />
-    </div>
+
 
     <div class="col-6">
       <chart-component title="Progressi Mesociclo" type="bar"
@@ -91,8 +92,6 @@ import chartData from '@/test/test_data/chartData.json';
 
 import chartData2 from '@/test/test_data/chartData2.json';
 import chartOptions2 from '@/test/test_data/chartOptions2.json';
-import chartData1RM from '@/test/test_data/chartData1RM.json';
-import chartOptions1RM from '@/test/test_data/chartOptions1RM.json';
 
 import barLineChartData from '@/test/test_data/barLineChartData.json';
 import barLineChartOptions from '@/test/test_data/barLineChartOptions.json';
@@ -110,19 +109,26 @@ import dataA2 from '@/test/test_data/dataA2.json';
 import dataA3 from '@/test/test_data/dataA3.json';
 import optionA from '@/test/test_data/optionA.json';
 
+// B
 import dataB from '@/test/test_data/dataB.json';
 import optionB from '@/test/test_data/optionB.json';
 
+// C
 import dataC from '@/test/test_data/dataC.json';
 import optionC from '@/test/test_data/optionC.json';
 
+// D
 import dataD from '@/test/test_data/dataD.json';
 import optionD from '@/test/test_data/optionD.json';
-
 import dataD2 from '@/test/test_data/dataD2.json';
 import optionD2 from '@/test/test_data/optionD2.json';
 
+// E
 import VGridComponent from '@/components/charts/VGridComponent.vue';
+
+// F
+import dataF from '@/test/test_data/dataF.json';
+import optionF from '@/test/test_data/optionF.json';
 
 onMounted(() => {
   dataInput.registeredFitness.forEach((el) => el.date = new Date(el.date))
