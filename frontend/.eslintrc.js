976707757a3595e4d17c9169921d4b3cd0d8f00b
Lorenzo <lorenzobof@outlook.it>
2023-08-01 15:22:20 +0200

    Tried to solve ESLint errors adding config file, but wasnt able

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    parser: 'vue-eslint-parser',
+    plugins: ['vue'],
+    extends: ['plugin:vue/vue3-essential', 'eslint:recommended'],
+    rules: {
+        'vue/multi-word-component-names': 'off',
+    },
+  };
+  
