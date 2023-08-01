1dd104daffcfaeedd788c316706020e0b7c838c4
AleLuppi <alelup@yahoo.it>
2023-08-01 17:28:50 +0200

    Move eslint config in a separate file
    Was originally inside main "package.json" file.
    Add a rule to allow, with warning, single-word components name.

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,9 +1,18 @@
 module.exports = {
-    parser: 'vue-eslint-parser',
-    plugins: ['vue'],
-    extends: ['plugin:vue/vue3-essential', 'eslint:recommended'],
-    rules: {
-        'vue/multi-word-component-names': 'off',
+    "root": true,
+    "env": {
+        "node": true,
+        "vue/setup-compiler-macros": true
     },
-  };
-  
+    "extends": [
+        "plugin:vue/vue3-essential",
+        "eslint:recommended",
+        "@vue/typescript"
+    ],
+    "parserOptions": {
+        "parser": "@typescript-eslint/parser"
+    },
+    "rules": {
+        "vue/multi-word-component-names": "warn"
+    }
+};
