5b6974b5782bd3ae00adbec489a1d7426fe35443
Lorenzo <lorenzobof@outlook.it>
2023-08-02 09:15:55 +0200

    Add DoughnutChart component template Added new chart as template similar to ChartComponent. For the moment I kept it separated for simplicity. In a second moment I will clean up all the unused components.

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,8 +1,21 @@
 <template>
   <div class="home">
-    <p>This is home</p> 
+    <DoughnutChart :data="chartData" />
   </div>  
 </template>
 
+
 <script setup>
+import { ref } from 'vue';
+import DoughnutChart from '@/components/charts/DoughnutChartComponent.vue'; 
+
+const chartData = ref({
+  labels: ['Label 1', 'Label 2', 'Label 3'],
+  datasets: [
+    {
+      data: [30, 50, 20],
+      backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
+    },
+  ],
+});
 </script>
