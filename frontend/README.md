0820aefa43d8d9e96f5ecffcc8de02740cc8e33c
AleLuppi <alelup@yahoo.it>
2023-09-08 00:05:53 +0200

    Add firebase support
    Add firebase support with
    - hosting
    - firestore
    - functions (not configured yet)
    Add documentation in ReadMe.md on how to deploy with Firebase

--- frontend/README.md
+++ frontend/README.md
@@ -31,5 +31,16 @@ Run linter to clean code. Run it before commit (or enable auto-lint on update).
 npm run lint
 ```
 
-### Customize configuration
+### Deploy on Firebase
+Test web app dedicated channel. Deploy it and get the temporary link (`CHANNEL_ID` can be any valid name).
+```
+firebase hosting:channel:deploy CHANNEL_ID
+```
+Deploy the web app Firebase's hosting domains (_login is required via_ `firebase login`).
+```
+firebase deploy --only hosting
+```
+Optionally, it is possible to deploy other services too, separated by commas. See here for more details: [https://firebase.google.com/docs/cli#partial_deploys](https://firebase.google.com/docs/cli#partial_deploys).
+
+## Customize configuration
 To update Vue CLI configuration, see [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ frontend/src/firebase.ts
@@ -0,0 +1,34 @@
+// Import the required functions from the SDKs
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+// Other SDKs for Firebase products at following link
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDQMm4d1r323z3uq_sAxVTqh9M49gicngw",
+  authDomain: "openstrength.firebaseapp.com",
+  projectId: "openstrength",
+  storageBucket: "openstrength.appspot.com",
+  messagingSenderId: "512957046617",
+  appId: "1:512957046617:web:72b3115ebab7d33a217648",
+  measurementId: "G-XB71BH7FWM"
+};
+
+
+/* ----- Initialization ----- */
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
+
+// Initialize Analytics and get a reference to the service
+export const analytics = getAnalytics(app);
+
+// Initialize Firebase Authentication and get a reference to the service
+export const auth = getAuth(app);
+
+// Initialize Cloud Firestore and get a reference to the service
+export const db = getFirestore(app);
