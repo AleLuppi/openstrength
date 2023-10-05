b4abe4598194cfc28ed72d0443ac401fdb41012c
Lorenzo <lorenzobof@outlook.it>
2023-10-05 16:07:07 +0200

    Add i18n to onboarding

--- frontend/src/views/UserOnboardingView.vue
+++ frontend/src/views/UserOnboardingView.vue
@@ -8,15 +8,12 @@
       animated
     >
       <!-- Step 1-->
-      <q-step :name="1" title="Step 1" icon="settings" :done="step > 1">
+      <q-step :name="1" :title="step1Title" icon="settings" :done="step > 1">
         <div class="image-container">
           <img :src="imageSrc" alt="Logo" class="centered-image" />
         </div>
-        <h3 class="text-center">Tell us more</h3>
-        <p class="text-center">
-          How will you be using the app? Select one. Note: if you want to both
-          train and be trained select coach.
-        </p>
+        <h3 class="text-center">{{ $t("user.onboarding.title_step1") }}</h3>
+        <p class="text-center">{{ $t("user.onboarding.subtitle_step1") }}</p>
 
         <div class="row justify-center q-gutter-sm">
           <q-btn
@@ -35,18 +32,16 @@
       <!-- Step 2-->
       <q-step
         :name="2"
-        title="Step 2"
-        caption="Optional"
+        :title="step2Title"
+        :caption="step2Caption"
         icon="info"
         :done="step > 2"
       >
         <div class="image-container">
           <img :src="imageSrc" alt="Logo" class="centered-image" />
         </div>
-        <h3 class="text-center">Let's meet better!</h3>
-        <p class="text-center">
-          Select the most common disciplines among your athletes:
-        </p>
+        <h3 class="text-center">{{ $t("user.onboarding.title_step2") }}</h3>
+        <p class="text-center">{{ $t("user.onboarding.subtitle_step2") }}</p>
 
         <div class="row justify-center">
           <q-list>
@@ -67,18 +62,16 @@
       <!-- Step 3-->
       <q-step
         :name="3"
-        title="Step 3"
-        caption="Optional"
+        :title="step3Title"
+        :caption="step3Caption"
         icon="info"
         :done="step > 3"
       >
         <div class="image-container">
           <img :src="imageSrc" alt="Logo" class="centered-image" />
         </div>
-        <h3 class="text-center">We are almost there!</h3>
-        <p class="text-center">
-          Select the average number of athletes you work with at the same time:
-        </p>
+        <h3 class="text-center">{{ $t("user.onboarding.title_step3") }}</h3>
+        <p class="text-center">{{ $t("user.onboarding.subtitle_step3") }}</p>
 
         <div class="row justify-center">
           <q-list>
@@ -105,34 +98,33 @@
               exportSelectedRole(), onAthleteRedirect(), $refs.stepper.next()
             "
             color="primary"
-            label="Continue"
-          />
+          >
+            {{ $t("common.continue") }}
+          </q-btn>
           <q-btn
             v-else-if="step === 2"
             @click="exportSelectedSports(), $refs.stepper.next()"
             color="primary"
-            label="Continue"
-          />
+          >
+            {{ $t("common.continue") }}
+          </q-btn>
           <q-btn
             v-else-if="step === 3"
             @click="exportSelectedAthletesRange(), onFinishRedirect()"
             color="primary"
-            label="Finish"
-          />
-          <q-btn
-            v-else
-            @click="$refs.stepper.next()"
-            color="primary"
-            :label="step === 3 ? 'Finish' : 'Continue'"
-          />
+          >
+            {{ $t("common.finish") }}
+          </q-btn>
+
           <q-btn
             v-if="step > 1"
             flat
             color="primary"
             @click="$refs.stepper.previous()"
-            label="Back"
             class="q-ml-sm"
-          />
+          >
+            {{ $t("common.back") }}
+          </q-btn>
         </q-stepper-navigation>
       </template>
     </q-stepper>
@@ -144,6 +136,15 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import { useRouter } from "vue-router";
+import { useI18n } from "vue-i18n";
+
+const i18n = useI18n();
+const step1Title = i18n.t("user.onboarding.bartitle_step1");
+//const step1Caption = i18n.t("user.onboarding.barcaption_step1");
+const step2Title = i18n.t("user.onboarding.bartitle_step2");
+const step2Caption = i18n.t("user.onboarding.barcaption_step2");
+const step3Title = i18n.t("user.onboarding.bartitle_step3");
+const step3Caption = i18n.t("user.onboarding.barcaption_step3");
 
 const imageSrc = ref("/assets/logo.png");
 
@@ -152,16 +153,39 @@ const router = useRouter();
 const step = ref(1);
 
 const buttonsSports = ref([
-  { id: 1, label: "Powerlifting", selected: ref(false) },
-  { id: 2, label: "Bodybuilding", selected: ref(false) },
-  { id: 3, label: "Calisthenics", selected: ref(false) },
-  { id: 4, label: "Powerbuilding", selected: ref(false) },
-  { id: 5, label: "Crossfit", selected: ref(false) },
-  { id: 6, label: "Weightlifting", selected: ref(false) },
-  { id: 7, label: "Fitness", selected: ref(false) },
-  { id: 8, label: "Streetlifting", selected: ref(false) },
-  { id: 9, label: "Generic", selected: ref(false) },
-  { id: 10, label: "Other", selected: ref(false) },
+  {
+    id: 1,
+    label: i18n.t("user.onboarding.powerlifting"),
+    selected: ref(false),
+  },
+  {
+    id: 2,
+    label: i18n.t("user.onboarding.bodybuilding"),
+    selected: ref(false),
+  },
+  {
+    id: 3,
+    label: i18n.t("user.onboarding.calisthenics"),
+    selected: ref(false),
+  },
+  {
+    id: 4,
+    label: i18n.t("user.onboarding.powerbuilding"),
+    selected: ref(false),
+  },
+  { id: 5, label: i18n.t("user.onboarding.crossfit"), selected: ref(false) },
+  {
+    id: 6,
+    label: i18n.t("user.onboarding.weightlifting"),
+    selected: ref(false),
+  },
+  { id: 7, label: i18n.t("user.onboarding.fitness"), selected: ref(false) },
+  {
+    id: 8,
+    label: i18n.t("user.onboarding.streetlifting"),
+    selected: ref(false),
+  },
+  { id: 10, label: i18n.t("common.other"), selected: ref(false) },
 ]);
 
 const buttonsAthletesRange = ref([
@@ -173,8 +197,8 @@ const buttonsAthletesRange = ref([
 ]);
 
 const buttonsRoles = ref([
-  { id: 16, label: "Athlete", selected: ref(false) },
-  { id: 17, label: "Coach", selected: ref(false) },
+  { id: 16, label: i18n.t("user.onboarding.athlete"), selected: ref(false) },
+  { id: 17, label: i18n.t("user.onboarding.coach"), selected: ref(false) },
 ]);
 
 // BUTTON SELECTION METHODS
@@ -231,7 +255,7 @@ const onAthleteRedirect = () => {
     .filter((button) => button.selected)
     .map((button) => button.label);
 
-  if (selectedLabels.includes("Athlete")) {
+  if (selectedLabels.includes(i18n.t("user.onboarding.athlete"))) {
     router.push({ name: "comingsoon" });
   }
 };
