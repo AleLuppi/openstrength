86a941bfde4c4b3bf45078e3e94e4a28c8187ca7
Lorenzo Boffa <85372151+LorenzoBoffa@users.noreply.github.com>
2023-10-09 21:37:56 +0200

    Add tab panel in library view
    The second tab is to manage, created and saved programs

--- frontend/src/views/LibraryView.vue
+++ frontend/src/views/LibraryView.vue
@@ -3,178 +3,212 @@
     <!-- Title -->
     <h2 class="col">{{ $t("layout.views.library_title") }}</h2>
 
-    <!-- Excercise cards -->
-    <div class="row q-col-gutter-x-md">
-      <!-- List exercises -->
-      <div class="col-12 col-sm-6">
-        <q-card>
-          <q-card-section>
-            <h6>{{ $t("coach.excercise_management.list_title") }}</h6>
+    <!-- Navigation -->
+    <q-tabs v-model="selectedTab" class="text-dark q-pa-md">
+      <q-tab
+        v-for="tab in ['exercises', 'programs']"
+        :key="tab"
+        :name="tab"
+        :label="$t('layout.views.librarytab_' + tab)"
+      />
+    </q-tabs>
 
-            <div class="row q-gutter-x-md items-center">
-              <os-input
-                v-model="searchExercise"
-                :placeholder="$t('coach.excercise_management.list_search')"
-                hide-bottom-space
-                debounce="500"
-                class="col"
+    <!-- First Tab: Exercises -->
+    <q-tab-panels v-model="selectedTab">
+      <q-tab-panel name="exercises">
+        <!-- Excercise cards -->
+        <div class="row q-col-gutter-x-md">
+          <!-- List exercises -->
+          <div class="col-12 col-sm-6">
+            <q-card>
+              <q-card-section>
+                <h6>{{ $t("coach.excercise_management.list_title") }}</h6>
+
+                <div class="row q-gutter-x-md items-center">
+                  <os-input
+                    v-model="searchExercise"
+                    :placeholder="$t('coach.excercise_management.list_search')"
+                    hide-bottom-space
+                    debounce="500"
+                    class="col"
+                  >
+                    <template v-slot:prepend>
+                      <q-icon name="search" />
+                    </template>
+                  </os-input>
+
+                  <q-btn
+                    :icon="$q.screen.gt.sm ? undefined : 'add'"
+                    :label="
+                      $q.screen.gt.sm
+                        ? $t('coach.excercise_management.add_button')
+                        : undefined
+                    "
+                    @click="onAdd"
+                  />
+                </div>
+              </q-card-section>
+
+              <q-separator />
+
+              <tableExerciseLibrary
+                :exercises="exercises"
+                :on-variant-update="onVariantUpdate"
+                :on-exercise-delete="onExerciseDelete"
+                :on-variant-delete="onExerciseVariantDelete"
               >
-                <template v-slot:prepend>
-                  <q-icon name="search" />
-                </template>
-              </os-input>
+              </tableExerciseLibrary>
+            </q-card>
+          </div>
 
+          <!-- Create or update exercise -->
+          <div class="col-12 col-sm-6">
+            <q-card v-if="showExerciseForm" class="col-12 col-sm-6">
+              <q-card-section>
+                <h6>
+                  {{
+                    $t(
+                      "coach.excercise_management." +
+                        (selectedExercise ? "update" : "add"),
+                    )
+                  }}
+                </h6>
+              </q-card-section>
+
+              <q-card-section>
+                <q-form @submit="onSubmit">
+                  <div class="row q-col-gutter-x-md">
+                    <os-input
+                      v-model="exerciseName"
+                      :label="$t('coach.excercise_management.exercise_name')"
+                      class="col-12 col-md-6"
+                    />
+                    <os-select
+                      v-model="variantName"
+                      :label="$t('coach.excercise_management.exercise_variant')"
+                      use-input
+                      :options="
+                        exerciseVariants?.map((variant) => variant.name)
+                      "
+                      class="col-12 col-md-6"
+                    />
+                    <os-select
+                      v-model="exerciseMuscleGroups"
+                      :label="
+                        $t('coach.excercise_management.exercise_musclegroups')
+                      "
+                      use-input
+                      :options="exerciseMuscleGroupsOptions"
+                      multiple
+                      class="col-12"
+                    />
+                    <os-select
+                      v-model="exerciseLoadType"
+                      :label="
+                        $t('coach.excercise_management.exercise_loadtype')
+                      "
+                      :options="
+                        (
+                          Object.keys(
+                            ExerciseLoadType,
+                          ) as (keyof typeof ExerciseLoadType)[]
+                        ).map((val) => ({
+                          label: $t(
+                            'coach.excercise_management.exercise_loadtypes.' +
+                              val,
+                          ),
+                          value: val,
+                        }))
+                      "
+                      emit-value
+                      map-options
+                      class="col-12 col-md-6"
+                    />
+                    <os-select
+                      v-model="variantEquipment"
+                      :label="
+                        $t('coach.excercise_management.exercise_equipment')
+                      "
+                      use-input
+                      :options="exerciseEquipmentOptions"
+                      multiple
+                      class="col-12 col-md-6"
+                    />
+                    <os-input
+                      v-model="variantVideo"
+                      :label="$t('coach.excercise_management.exercise_video')"
+                      class="col-12"
+                    />
+                    <os-input
+                      v-model="variantDescription"
+                      :label="
+                        $t('coach.excercise_management.exercise_description')
+                      "
+                      type="textarea"
+                      class="col-12"
+                    />
+                  </div>
+
+                  <q-btn
+                    type="submit"
+                    :label="$t('coach.excercise_management.add_proceed')"
+                    class="full-width"
+                  ></q-btn>
+                </q-form>
+              </q-card-section>
+            </q-card>
+          </div>
+        </div>
+
+        <!-- Dialog delete an exercise or variant -->
+        <q-dialog
+          v-model="showDeleteDialog"
+          @hide="
+            deletingExercise = undefined;
+            deletingVariant = undefined;
+          "
+        >
+          <q-card class="q-pa-sm dialog-min-width">
+            <q-card-section class="row items-center q-pb-none">
+              <p>
+                {{
+                  deletingExercise
+                    ? $t("coach.excercise_management.delete_exercise_confirm", {
+                        exercise: deletingExercise?.name,
+                      })
+                    : $t("coach.excercise_management.delete_variant_confirm", {
+                        variant: deletingVariant?.name,
+                        exercise: deletingVariant?.exercise?.name,
+                      })
+                }}
+              </p>
+            </q-card-section>
+
+            <q-card-actions align="right">
               <q-btn
-                :icon="$q.screen.gt.sm ? undefined : 'add'"
-                :label="
-                  $q.screen.gt.sm
-                    ? $t('coach.excercise_management.add_button')
-                    : undefined
-                "
-                @click="onAdd"
+                flat
+                :label="$t('common.cancel')"
+                type="reset"
+                v-close-popup
               />
-            </div>
-          </q-card-section>
-
-          <q-separator />
-
-          <tableExerciseLibrary
-            :exercises="exercises"
-            :on-variant-update="onVariantUpdate"
-            :on-exercise-delete="onExerciseDelete"
-            :on-variant-delete="onExerciseVariantDelete"
-          >
-          </tableExerciseLibrary>
-        </q-card>
-      </div>
-
-      <!-- Create or update exercise -->
-      <div class="col-12 col-sm-6">
-        <q-card v-if="showExerciseForm" class="col-12 col-sm-6">
-          <q-card-section>
-            <h6>
-              {{
-                $t(
-                  "coach.excercise_management." +
-                    (selectedExercise ? "update" : "add"),
-                )
-              }}
-            </h6>
-          </q-card-section>
-
-          <q-card-section>
-            <q-form @submit="onSubmit">
-              <div class="row q-col-gutter-x-md">
-                <os-input
-                  v-model="exerciseName"
-                  :label="$t('coach.excercise_management.exercise_name')"
-                  class="col-12 col-md-6"
-                />
-                <os-select
-                  v-model="variantName"
-                  :label="$t('coach.excercise_management.exercise_variant')"
-                  use-input
-                  :options="exerciseVariants?.map((variant) => variant.name)"
-                  class="col-12 col-md-6"
-                />
-                <os-select
-                  v-model="exerciseMuscleGroups"
-                  :label="
-                    $t('coach.excercise_management.exercise_musclegroups')
-                  "
-                  use-input
-                  :options="exerciseMuscleGroupsOptions"
-                  multiple
-                  class="col-12"
-                />
-                <os-select
-                  v-model="exerciseLoadType"
-                  :label="$t('coach.excercise_management.exercise_loadtype')"
-                  :options="
-                    (
-                      Object.keys(
-                        ExerciseLoadType,
-                      ) as (keyof typeof ExerciseLoadType)[]
-                    ).map((val) => ({
-                      label: $t(
-                        'coach.excercise_management.exercise_loadtypes.' + val,
-                      ),
-                      value: val,
-                    }))
-                  "
-                  emit-value
-                  map-options
-                  class="col-12 col-md-6"
-                />
-                <os-select
-                  v-model="variantEquipment"
-                  :label="$t('coach.excercise_management.exercise_equipment')"
-                  use-input
-                  :options="exerciseEquipmentOptions"
-                  multiple
-                  class="col-12 col-md-6"
-                />
-                <os-input
-                  v-model="variantVideo"
-                  :label="$t('coach.excercise_management.exercise_video')"
-                  class="col-12"
-                />
-                <os-input
-                  v-model="variantDescription"
-                  :label="$t('coach.excercise_management.exercise_description')"
-                  type="textarea"
-                  class="col-12"
-                />
-              </div>
-
               <q-btn
-                type="submit"
-                :label="$t('coach.excercise_management.add_proceed')"
-                class="full-width"
-              ></q-btn>
-            </q-form>
-          </q-card-section>
-        </q-card>
-      </div>
-    </div>
-
-    <!-- Dialog delete an exercise or variant -->
-    <q-dialog
-      v-model="showDeleteDialog"
-      @hide="
-        deletingExercise = undefined;
-        deletingVariant = undefined;
-      "
-    >
-      <q-card class="q-pa-sm dialog-min-width">
-        <q-card-section class="row items-center q-pb-none">
-          <p>
-            {{
-              deletingExercise
-                ? $t("coach.excercise_management.delete_exercise_confirm", {
-                    exercise: deletingExercise?.name,
-                  })
-                : $t("coach.excercise_management.delete_variant_confirm", {
-                    variant: deletingVariant?.name,
-                    exercise: deletingVariant?.exercise?.name,
-                  })
-            }}
-          </p>
-        </q-card-section>
-
-        <q-card-actions align="right">
-          <q-btn flat :label="$t('common.cancel')" type="reset" v-close-popup />
-          <q-btn
-            :label="$t('coach.athlete_management.list.add_proceed')"
-            @click="
-              if (deletingExercise) deleteExercise(deletingExercise);
-              if (deletingVariant) deleteVariant(deletingVariant);
-            "
-            v-close-popup
-          />
-        </q-card-actions>
-      </q-card>
-    </q-dialog>
+                :label="$t('coach.athlete_management.list.add_proceed')"
+                @click="
+                  if (deletingExercise) deleteExercise(deletingExercise);
+                  if (deletingVariant) deleteVariant(deletingVariant);
+                "
+                v-close-popup
+              />
+            </q-card-actions>
+          </q-card>
+        </q-dialog>
+      </q-tab-panel>
+      <!-- Second tab: Programs -->
+      <q-tab-panel name="programs">
+        <div class="text-h5">TODO</div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      </q-tab-panel>
+    </q-tab-panels>
   </div>
 </template>
 
@@ -201,6 +235,7 @@ const user = useUserStore();
 const coachInfo = useCoachInfoStore();
 
 // Set ref
+const selectedTab = ref("exercises"); // main tab to show
 const showExerciseForm = ref(false);
 const showDeleteDialog = ref(false);
 const deletingExercise = ref<Exercise>();
