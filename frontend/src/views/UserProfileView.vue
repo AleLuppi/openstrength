e182bb997ff17e0a9c368fe7cd665bed57e8cdc0
Lorenzo Boffa <85372151+LorenzoBoffa@users.noreply.github.com>
2023-10-12 22:20:10 +0200

    Add user info in profile as #59
    To be finished: missing role and status

--- frontend/src/views/UserProfileView.vue
+++ frontend/src/views/UserProfileView.vue
@@ -6,12 +6,23 @@
       <h6 class="q-mx-md q-py-md">{{ $t("user.profile.info_title") }}</h6>
 
       <div class="row justify-between">
-        <!-- Profile Image-->
+        <!-- Profile Image -->
         <q-section class="column align-left q-mx-md">
           <q-icon
+            v-if="!photoUrl"
             name="account_circle"
             size="6em"
             class="justify-center card-icon q-ma-sm"
+          />
+          <q-img
+            v-else
+            :src="photoUrl"
+            size="6em"
+            width="96px"
+            height="96px"
+            class="justify-center q-ma-sm"
+            style="border-radius: 50%"
+            @error="photoUrl = undefined"
           />
         </q-section>
         <!-- Name and Surname -->
@@ -19,34 +30,34 @@
           <p class="text-h6 text-left">
             {{ $t("user.profile.name") }}
           </p>
-          <p class="text-left">Lorenzo Amatore</p>
+          <p class="text-left">{{ user.displayName }}</p>
         </q-section>
         <!-- Email -->
         <q-section class="column align-center q-mx-md">
           <p class="text-h6 text-left">
             {{ $t("user.profile.email") }}
           </p>
-          <p class="text-left">lorenzmataore@gmail.com</p>
+          <p class="text-left">{{ user.email }}</p>
         </q-section>
         <!-- Role: athlete or coach -->
         <q-section class="column align-center q-mx-md">
           <p class="text-h6 text-left">
             {{ $t("user.profile.role") }}
           </p>
-          <p class="text-left text-primary">Coach</p>
+          <p class="text-left text-primary">to be done</p>
         </q-section>
         <!-- Status: active or inactive-->
         <q-section class="column align-center q-mx-md">
           <p class="text-h6 text-left">
             {{ $t("user.profile.status") }}
           </p>
-          <p class="text-left text-green">Active</p>
+          <p class="text-left text-green">to be done</p>
         </q-section>
         <q-section class="column align-center q-mx-md q-mr-xl">
           <p class="text-h6 text-left">
             {{ $t("user.profile.uid") }}
           </p>
-          <p class="text-left">HSFSODA904730HHK</p>
+          <p class="text-left">{{ user.uid }}</p>
         </q-section>
       </div>
 
@@ -66,14 +77,20 @@
 </template>
 
 <script setup lang="ts">
-// TODO: pass actual user info
 import { useQuasar } from "quasar";
 import { useI18n } from "vue-i18n";
 import { doSignOut } from "@/helpers/users/auth";
+import { useUserStore } from "@/stores/user";
 
 // Init plugin
 const $q = useQuasar();
 const i18n = useI18n();
+
+// Get user state
+const user = useUserStore();
+
+// Profile picture
+const photoUrl = user.photoUrl;
 
 /**
  * Sign out user
