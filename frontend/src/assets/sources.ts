569f495dac5e613f9740f532469cc1e00699184c
Lorenzo Boffa <85372151+LorenzoBoffa@users.noreply.github.com>
2023-11-04 17:35:40 +0100

    Add final svg logos
    Added also logos for dark theme even if not needed right now.

--- frontend/src/assets/sources.ts
+++ frontend/src/assets/sources.ts
@@ -1,2 +1,4 @@
 export const logoFullImage = require("@/assets/logo.svg");
 export const logo404Image = require("@/assets/logo404.svg");
+export const logoFullImageDark = require("@/assets/logo_dark.svg");
+export const logo404ImageDark = require("@/assets/logo404_dark.svg");
