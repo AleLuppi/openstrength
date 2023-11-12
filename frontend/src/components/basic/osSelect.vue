365e963f1e01a78302c70288be6c1b486dc7dcca
Lorenzo Boffa <85372151+LorenzoBoffa@users.noreply.github.com>
2023-11-12 23:21:20 +0100

    Improve os-select to match os-input

--- frontend/src/components/basic/osSelect.vue
+++ frontend/src/components/basic/osSelect.vue
@@ -1,14 +1,14 @@
 <template>
   <div v-on="$attrs">
     <!-- Title -->
-    <h6
+    <p
       v-if="props.label"
-      class="q-my-none text-input-top-label text-uppercase text-weight-medium"
+      class="text-input-top-label text-uppercase text-weight-medium"
       :class="{ 'input-required': props.required }"
       style="line-height: 1.6em"
     >
       {{ props.label }}
-    </h6>
+    </p>
 
     <!-- Styled input -->
     <q-select
