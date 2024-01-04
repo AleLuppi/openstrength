b5dd7648a6d49d3185eba1d03735f0ac6b4a5fa5
Lorenzo Boffa <85372151+LorenzoBoffa@users.noreply.github.com>
2024-01-04 22:46:13 +0100

    Improve UI/UX in athletes and library
    Again, this is the Minimum Viable Modification to obtain the wanted result. Now the controls are placed in the reading order and with better CTA

--- frontend/src/quasar-user-options.ts
+++ frontend/src/quasar-user-options.ts
@@ -1,6 +1,8 @@
 import "./styles/quasar.scss";
 import "@quasar/extras/roboto-font/roboto-font.css";
 import "@quasar/extras/material-icons/material-icons.css";
+import "@quasar/extras/material-symbols-outlined/material-symbols-outlined.css";
+
 import "@quasar/extras/fontawesome-v6/fontawesome-v6.css";
 import { Notify } from "quasar";
 
