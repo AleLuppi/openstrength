712570459fa9fde38bee32b2767fc2c219ef3edf
Lorenzo Boffa <85372151+LorenzoBoffa@users.noreply.github.com>
2024-01-07 13:11:58 +0100

    Add calculations from RPE table

--- frontend/src/helpers/charts/chartDatasetComputations.ts
+++ frontend/src/helpers/charts/chartDatasetComputations.ts
@@ -6,7 +6,7 @@ import { ProgramLine } from "../programs/program";
  * Each value is a percentage of 1RM
  * Column index define the reps, row index define the rpe
  */
-const rpeRepsTable: number[][] = [
+export const rpeRepsTable: number[][] = [
   //1   2   3   4   5   6   7   8   9   10  11  12  13  14  15  reps
   [100, 96, 92, 89, 86, 84, 81, 79, 76, 74, 71, 69, 66, 64, 61], // 10
   [98, 94, 91, 88, 85, 82, 80, 77, 75, 72, 70, 67, 65, 62, 60], // 9.5
@@ -21,7 +21,7 @@ const rpeRepsTable: number[][] = [
 /**
  * Method to compute the percentage of 1RM [%] from the rpe-reps table
  * @param reps repetition number from 1 to 15
- * @param rpe rep from 6.5 to 10 at 0.5 increments
+ * @param rpe rpe from 6.5 to 10 at 0.5 increments
  * @param rpeTable actual instance of rpe table (2D array)
  * @returns Given a reps number and an rpe, returns the % of the 1RM as a number from 1 to 100
  */
@@ -42,7 +42,78 @@ export function calculatePercentage1RM(
   return percentage1RM;
 }
 
-//TODO: substitute BaseValue with Value
+/**
+ * Method to compute the number of reps given a % of the RM and rpe
+ * @param percentage from 0 to 100, indicates load/1RM
+ * @param rpe rpe from 6.5 to 10 at 0.5 increments
+ * @param rpeTable actual instance of rpe table (2D array)
+ * @returns number of reps for that specific load% and RPE
+ */
+export function calculateRepsFromTable(
+  percentage: number,
+  rpe: number,
+  rpeTable: number[][],
+): number | undefined {
+  if (
+    percentage < 0 ||
+    percentage > 100 ||
+    rpe < 6.5 ||
+    rpe > 10 ||
+    !rpeTable
+  ) {
+    console.error("Invalid percentage, RPE values, or RPE table.");
+    return undefined;
+  }
+
+  const rpeIndex = 7 - Math.round((rpe - 6.5) * 2);
+  const row = rpeTable[rpeIndex];
+
+  // Iterate over the row to find the closest value to the provided percentage
+  const closestIndex = row
+    .map((value, index) => ({ index, diff: Math.abs(value - percentage) }))
+    .reduce((min, current) => (current.diff < min.diff ? current : min)).index;
+
+  // Reps value is the index + 1
+  const reps = closestIndex + 1;
+
+  return reps;
+}
+
+/**
+ * Method to compute the rpe given a % of the RM and reps
+ * @param percentage from 0 to 100, indicates load/1RM
+ * @param reps reps number
+ * @param rpeTable actual instance of rpe table (2D array)
+ * @returns rpe from 6.5 to 10 at 0.5 increments
+ */
+export function calculateRpeFromTable(
+  percentage: number,
+  reps: number,
+  rpeTable: number[][],
+): number | undefined {
+  if (
+    percentage < 0 ||
+    percentage > 100 ||
+    reps < 1 ||
+    reps > 15 ||
+    !rpeTable
+  ) {
+    console.error("Invalid percentage, reps values, or RPE table.");
+    return undefined;
+  }
+
+  const repsIndex = reps - 1;
+  const column = rpeTable.map((row) => row[repsIndex]);
+
+  // Iterate over the row to find the closest value to the provided percentage
+  const closestIndex = column
+    .map((value, index) => ({ index, diff: Math.abs(value - percentage) }))
+    .reduce((min, current) => (current.diff < min.diff ? current : min)).index;
+
+  const rpe = 6.5 + 0.5 * (7 - closestIndex);
+
+  return rpe;
+}
 
 /**
  * Fallback method when no available computational methods are present
