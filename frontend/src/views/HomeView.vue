ca763ee9fce845b371a58c096f0ad987552ea270
AleLuppi <alelup@yahoo.it>
2024-01-23 12:37:57 +0100

    Use a single page element in home view

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,223 +1,214 @@
 <template>
   <q-page
-    v-if="user.role == UserRole.athlete"
     class="q-mx-auto q-px-md q-py-lg text-center column justify-center items-stretch"
   >
-    <!-- Show coming soon in case of athlete -->
-    <img :src="logoFullImage" :srcset="logoFullImage + ' 1.2x'" alt="Logo" />
-    <h2 class="text-center">
-      {{ $t("comingsoon.title") }}
-    </h2>
-    <p>
-      {{ $t("comingsoon.subtitle") }}
-    </p>
-  </q-page>
-
-  <q-page
-    v-else-if="user.role == UserRole.coach"
-    class="q-mx-auto q-px-md q-py-lg text-center column justify-center items-stretch"
-  >
-    <!-- Show homepage in case of coach -->
-    <div class="q-pa-md q-pb-lg q-mx-auto limit-max-width">
+    <div v-if="user.role == UserRole.athlete">
+      <!-- Show coming soon in case of athlete -->
+      <img :src="logoFullImage" :srcset="logoFullImage + ' 1.2x'" alt="Logo" />
       <h2 class="text-center">
-        {{
-          user.displayName
-            ? $t("homepage.welcome_with_name", {
-                name: user.displayName?.trim(),
-              })
-            : $t("homepage.welcome_without_name")
-        }}
+        {{ $t("comingsoon.title") }}
       </h2>
+      <p>
+        {{ $t("comingsoon.subtitle") }}
+      </p>
     </div>
 
-    <!-- Common actions -->
-    <div class="row q-gutter-lg justify-center items-center">
-      <router-link
-        v-for="buttonInfo in buttonsCoachAction"
-        :key="buttonInfo.to"
-        :to="{ name: buttonInfo.to }"
-        class="link-child"
-      >
-        <q-card
-          class="column items-center justify-center square-card q-hoverable text-center"
-          :class="
-            $q.screen.lt.sm
-              ? 'q-pa-xs square-card-mobile'
-              : 'q-pa-lg square-card'
-          "
-        >
-          <!-- Animate when on -->
-          <span class="q-focus-helper"></span>
+    <div v-else-if="user.role == UserRole.coach">
+      <!-- Show homepage in case of coach -->
+      <div class="q-pa-md q-pb-lg q-mx-auto limit-max-width">
+        <h2 class="text-center">
+          {{
+            user.displayName
+              ? $t("homepage.welcome_with_name", {
+                  name: user.displayName?.trim(),
+                })
+              : $t("homepage.welcome_without_name")
+          }}
+        </h2>
+      </div>
 
-          <!-- Icon, title, and subtitle -->
-          <div class="column justify-center items-center">
+      <!-- Common actions -->
+      <div class="row q-gutter-lg justify-center items-center">
+        <router-link
+          v-for="buttonInfo in buttonsCoachAction"
+          :key="buttonInfo.to"
+          :to="{ name: buttonInfo.to }"
+          class="link-child"
+        >
+          <q-card
+            class="column items-center justify-center square-card q-hoverable text-center"
+            :class="
+              $q.screen.lt.sm
+                ? 'q-pa-xs square-card-mobile'
+                : 'q-pa-lg square-card'
+            "
+          >
+            <!-- Animate when on -->
+            <span class="q-focus-helper"></span>
+
+            <!-- Icon, title, and subtitle -->
+            <div class="column justify-center items-center">
+              <q-icon
+                :name="buttonInfo.icon"
+                :size="$q.screen.lt.sm ? '3em' : '5em'"
+                color="icon-color"
+              />
+              <h4 class="text-center">
+                {{ $t(buttonInfo.title) }}
+              </h4>
+              <p class="q-px-md text-weight-light">
+                {{ $t(buttonInfo.subtitle) }}
+              </p>
+            </div>
+          </q-card>
+        </router-link>
+      </div>
+
+      <!-- Show dialog for 12Feb deadline -->
+      <q-dialog v-model="showDialogPresaleDeadline">
+        <q-card class="q-pa-sm">
+          <q-card-section class="items-center q-pb-none">
+            <div class="row items-start justify-between">
+              <h4>Stiamo per chiudere gli accessi!</h4>
+              <div>
+                <q-btn
+                  flat
+                  round
+                  dense
+                  icon="close"
+                  color="light-dark"
+                  v-close-popup
+                />
+              </div>
+            </div>
+
+            <p>
+              La piattaforma rimarrà aperta fino al 12 Febbraio, poi gli accessi
+              verranno chiusi per qualche mese.
+            </p>
+            <br />
+            <p>
+              Se non sei già dei nostri, contattaci dove preferisci per
+              ulteriori informazioni:
+            </p>
+
+            <div class="row justify-between items-start">
+              <div class="column justify-center items-center q-ma-sm">
+                <h6>Email</h6>
+                <a
+                  href="mailto:lorenzo.boffa06@gmail.com,lorenzo.amadori1996@gmail.com,aleluppi23@gmail.com?subject=OpenStrength: informazioni accesso piattaforma&body=Ciao, vorrei avere più informazioni."
+                >
+                  lorenzo.amadori1996@gmail.com <br />
+                  lorenzo.boffa06@gmail.com <br />
+                  aleluppi23@gmail.com</a
+                >
+              </div>
+
+              <div class="column justify-center items-center q-ma-sm">
+                <div class="row">
+                  <q-icon> </q-icon>
+                </div>
+                <h6>Whatsapp</h6>
+                <a
+                  href="https://wa.me/393405489016?text=Ciao%20vorrei%20più%20informazioni%20per%20l'accesso%20a%20OpenStrength"
+                >
+                  Lorenzo Amadori
+                </a>
+                <a
+                  href="https://wa.me/393468660263?text=Ciao%20vorrei%20più%20informazioni%20per%20l'accesso%20a%20OpenStrength"
+                >
+                  Lorenzo Boffa
+                </a>
+              </div>
+
+              <div class="column justify-center items-center q-ma-sm">
+                <h6>Instagram</h6>
+
+                <p><i>amalo96</i></p>
+                <p><i>loreboffa</i></p>
+              </div>
+            </div>
+          </q-card-section>
+        </q-card>
+      </q-dialog>
+    </div>
+
+    <div v-else-if="!user.isSignedIn">
+      <!-- Show call to action to unsigner user -->
+      <div class="q-pa-md q-pb-lg q-mx-auto limit-max-width">
+        <h2 class="text-center">
+          {{ $t("homepage.welcome_unsigned_user") }}
+        </h2>
+      </div>
+
+      <!-- Common actions -->
+      <div class="row q-gutter-lg justify-center items-center">
+        <router-link
+          v-for="buttonUnsignedUser in buttonsUnsigedAction"
+          :key="buttonUnsignedUser.to"
+          :to="{ name: buttonUnsignedUser.to }"
+          class="link-child"
+        >
+          <q-card
+            class="q-pa-lg column items-center justify-center square-card q-hoverable text-center"
+          >
+            <!-- Animate when on -->
+            <span class="q-focus-helper"></span>
+
+            <!-- Icon, title, and subtitle -->
             <q-icon
-              :name="buttonInfo.icon"
-              :size="$q.screen.lt.sm ? '3em' : '5em'"
+              :name="buttonUnsignedUser.icon"
+              size="5em"
               color="icon-color"
             />
-            <h4 class="text-center">
-              {{ $t(buttonInfo.title) }}
+            <h4 class="text-center'">
+              {{ $t(buttonUnsignedUser.title) }}
             </h4>
             <p class="q-px-md text-weight-light">
-              {{ $t(buttonInfo.subtitle) }}
+              {{ $t(buttonUnsignedUser.subtitle) }}
             </p>
-          </div>
-        </q-card>
-      </router-link>
+          </q-card>
+        </router-link>
+      </div>
     </div>
 
-    <!-- Show dialog for 12Feb deadline -->
-    <q-dialog v-model="showDialogPresaleDeadline">
-      <q-card class="q-pa-sm">
-        <q-card-section class="items-center q-pb-none">
-          <div class="row items-start justify-between">
-            <h4>Stiamo per chiudere gli accessi!</h4>
-            <div>
-              <q-btn
-                flat
-                round
-                dense
-                icon="close"
-                color="light-dark"
-                v-close-popup
-              />
-            </div>
-          </div>
+    <div v-else>
+      <!-- Show something else in all other cases -->
+      <div class="q-pa-md q-pb-lg q-mx-auto limit-max-width">
+        <h2 class="text-center">
+          {{ $t("homepage.welcome_unknown_user") }}
+        </h2>
+      </div>
 
-          <p>
-            La piattaforma rimarrà aperta fino al 12 Febbraio, poi gli accessi
-            verranno chiusi per qualche mese.
-          </p>
-          <br />
-          <p>
-            Se non sei già dei nostri, contattaci dove preferisci per ulteriori
-            informazioni:
-          </p>
-
-          <div class="row justify-between items-start">
-            <div class="column justify-center items-center q-ma-sm">
-              <h6>Email</h6>
-              <a
-                href="mailto:lorenzo.boffa06@gmail.com,lorenzo.amadori1996@gmail.com,aleluppi23@gmail.com?subject=OpenStrength: informazioni accesso piattaforma&body=Ciao, vorrei avere più informazioni."
-              >
-                lorenzo.amadori1996@gmail.com <br />
-                lorenzo.boffa06@gmail.com <br />
-                aleluppi23@gmail.com</a
-              >
-            </div>
-
-            <div class="column justify-center items-center q-ma-sm">
-              <div class="row">
-                <q-icon> </q-icon>
-              </div>
-              <h6>Whatsapp</h6>
-              <a
-                href="https://wa.me/393405489016?text=Ciao%20vorrei%20più%20informazioni%20per%20l'accesso%20a%20OpenStrength"
-              >
-                Lorenzo Amadori
-              </a>
-              <a
-                href="https://wa.me/393468660263?text=Ciao%20vorrei%20più%20informazioni%20per%20l'accesso%20a%20OpenStrength"
-              >
-                Lorenzo Boffa
-              </a>
-            </div>
-
-            <div class="column justify-center items-center q-ma-sm">
-              <h6>Instagram</h6>
-
-              <p><i>amalo96</i></p>
-              <p><i>loreboffa</i></p>
-            </div>
-          </div>
-        </q-card-section>
-      </q-card>
-    </q-dialog>
-    <div></div>
-  </q-page>
-
-  <q-page
-    v-else-if="!user.isSignedIn"
-    class="q-mx-auto q-px-md q-py-lg text-center column justify-center items-stretch"
-  >
-    <!-- Show call to action to unsigner user -->
-    <div class="q-pa-md q-pb-lg q-mx-auto limit-max-width">
-      <h2 class="text-center">
-        {{ $t("homepage.welcome_unsigned_user") }}
-      </h2>
-    </div>
-
-    <!-- Common actions -->
-    <div class="row q-gutter-lg justify-center items-center">
-      <router-link
-        v-for="buttonUnsignedUser in buttonsUnsigedAction"
-        :key="buttonUnsignedUser.to"
-        :to="{ name: buttonUnsignedUser.to }"
-        class="link-child"
-      >
+      <!-- Action -->
+      <div class="row q-gutter-lg justify-center items-center">
         <q-card
-          class="q-pa-lg column items-center justify-center square-card q-hoverable text-center"
+          class="q-pa-lg column items-center justify-center square-card q-hoverable text-center cursor-pointer"
+          clickable
+          v-ripple
+          @click="$emit('request-global-dialog', 'onboarding')"
         >
           <!-- Animate when on -->
           <span class="q-focus-helper"></span>
 
           <!-- Icon, title, and subtitle -->
           <q-icon
-            :name="buttonUnsignedUser.icon"
-            size="5em"
+            name="fa-regular fa-circle-play"
+            :size="$q.screen.lt.sm ? '3em' : '5em'"
             color="icon-color"
           />
-          <h4 class="text-center'">
-            {{ $t(buttonUnsignedUser.title) }}
-          </h4>
+          <p
+            :class="
+              $q.screen.lt.md ? 'text-center text-h6' : 'text-center text-h4'
+            "
+          >
+            {{ $t("homepage.onboarding_title") }}
+          </p>
           <p class="q-px-md text-weight-light">
-            {{ $t(buttonUnsignedUser.subtitle) }}
+            {{ $t("homepage.onboarding_subtitle") }}
           </p>
         </q-card>
-      </router-link>
-    </div>
-  </q-page>
-
-  <q-page
-    v-else
-    class="q-mx-auto q-px-md q-py-lg text-center column justify-center items-stretch"
-  >
-    <!-- Show something else in all other cases -->
-    <div class="q-pa-md q-pb-lg q-mx-auto limit-max-width">
-      <h2 class="text-center">
-        {{ $t("homepage.welcome_unknown_user") }}
-      </h2>
-    </div>
-
-    <!-- Action -->
-    <div class="row q-gutter-lg justify-center items-center">
-      <q-card
-        class="q-pa-lg column items-center justify-center square-card q-hoverable text-center cursor-pointer"
-        clickable
-        v-ripple
-        @click="$emit('request-global-dialog', 'onboarding')"
-      >
-        <!-- Animate when on -->
-        <span class="q-focus-helper"></span>
-
-        <!-- Icon, title, and subtitle -->
-        <q-icon
-          name="fa-regular fa-circle-play"
-          :size="$q.screen.lt.sm ? '3em' : '5em'"
-          color="icon-color"
-        />
-        <p
-          :class="
-            $q.screen.lt.md ? 'text-center text-h6' : 'text-center text-h4'
-          "
-        >
-          {{ $t("homepage.onboarding_title") }}
-        </p>
-        <p class="q-px-md text-weight-light">
-          {{ $t("homepage.onboarding_subtitle") }}
-        </p>
-      </q-card>
+      </div>
     </div>
   </q-page>
 </template>
