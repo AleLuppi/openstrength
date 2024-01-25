a5827998a1d8c4d89096d46dac87f0ea275514f1
Lorenzo Boffa <85372151+LorenzoBoffa@users.noreply.github.com>
2024-01-25 18:06:06 +0100

    Allow decimal places in maxlift value, closes #200

--- frontend/src/components/forms/FormMaxLift.vue
+++ frontend/src/components/forms/FormMaxLift.vue
@@ -28,8 +28,8 @@
         v-model="maxliftValue"
         :suffix="maxliftValueSuffix"
         :label="$t('coach.maxlift_management.fields.value')"
-        mask="#####"
         required
+        @input="filterInput()"
       ></os-input>
 
       <!-- Display estimated 1RM -->
@@ -234,6 +234,13 @@ function computeE1RM(
   maxliftObj.type = type;
 
   return estimate1RMfromNRM(maxliftObj);
+}
+
+/**
+ * Allows user to only use numbers and dot
+ */
+function filterInput() {
+  maxliftValue.value = maxliftValue.value?.replace(/[^0-9.]/g, "");
 }
 
 /**
