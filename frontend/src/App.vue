a9049cccba37e3da505ed334c419fefa10924eb5
AleLuppi <alelup@yahoo.it>
2024-02-11 23:55:09 +0100

    Delete presale dialog from app

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -123,99 +123,6 @@
       <q-dialog v-model="showDialogOnboarding">
         <UserOnboarding :on-submit="onOnboardingSubmit"></UserOnboarding>
       </q-dialog>
-
-      <!-- Show dialog for 12Feb deadline -->
-      <q-dialog
-        :modelValue="showDialogPresaleDeadline && userNotPaid"
-        @update:modelValue="(val) => (showDialogPresaleDeadline = val)"
-      >
-        <q-card class="q-pa-sm">
-          <q-card-section class="q-pb-none">
-            <h3 class="text-primary">
-              Ancora pochi giorni per provare OpenStrength!
-            </h3>
-            <q-btn
-              flat
-              round
-              dense
-              icon="close"
-              color="light-dark"
-              v-close-popup
-              style="position: absolute; top: 0; right: 0"
-            />
-          </q-card-section>
-          <q-card-section>
-            <p class="q-pb-md text-md">
-              Hai tempo fino al 12 Febbraio per provare liberamente
-              OpenStrength, poi limiteremo gli accessi per qualche mese.
-            </p>
-            <p class="q-pb-md text-md">
-              Vuoi continuare ad usare l'app?
-              <span class="text-bold">Scrivici!</span>
-            </p>
-
-            <div
-              v-for="[name, email, emailcc, whatsapp, instagram] in [
-                [
-                  'Lorenzo Amadori',
-                  'lorenzo.amadori1996@gmail.com',
-                  'lorenzo.boffa06@gmail.com',
-                  '393405489016',
-                  'amalo96',
-                ],
-                [
-                  'Lorenzo Boffa',
-                  'lorenzo.boffa06@gmail.com',
-                  'lorenzo.amadori1996@gmail.com',
-                  '393468660263',
-                  'loreboffa',
-                ],
-              ]"
-              class="row justify-between items-center q-col-gutter-md q-mb-md"
-              :key="name"
-            >
-              <div class="col">
-                <p class="text-bold" style="font-size: 1.1em">{{ name }}:</p>
-              </div>
-              <div>
-                <a
-                  :href="
-                    'mailto:' +
-                    email +
-                    '?subject=OpenStrength: accesso piattaforma' +
-                    '&body=Ciao, vorrei avere più informazioni riguardo all\'accesso a OpenStrength.' +
-                    (emailcc ? '&cc=' + emailcc : '')
-                  "
-                >
-                  {{ email }}
-                </a>
-              </div>
-              <div>
-                <q-btn
-                  icon="fab fah fa-whatsapp"
-                  round
-                  :href="
-                    'https://wa.me/' +
-                    whatsapp +
-                    '?text=Ciao!%20Vorrei%20qualche%20informazione%20in%20più%20riguardo%20a%20OpenStrength'
-                  "
-                  target="_blank"
-                  style="background-color: #25d366 !important"
-                ></q-btn>
-              </div>
-              <div>
-                <q-btn
-                  icon="fab fah fa-instagram"
-                  round
-                  :href="'https://www.instagram.com/' + instagram"
-                  target="_blank"
-                  style="background-color: #e1306c !important"
-                ></q-btn>
-              </div>
-            </div>
-          </q-card-section>
-        </q-card>
-      </q-dialog>
     </q-layout>
   </main>
 </template>
@@ -227,7 +134,6 @@ import {
   onBeforeMount,
   onMounted,
   defineAsyncComponent,
-  watch,
 } from "vue";
 import { useRoute } from "vue-router";
 import { User as FirebaseUser } from "firebase/auth";
@@ -282,47 +188,6 @@ const isLoading = ref(true);
 
 // Global dialogs ref
 const showDialogOnboarding = ref(false);
-const showDialogPresaleDeadline = ref(false);
-const userNotPaid = computed(() => {
-  return user.uid
-    ? ![
-        "KZ5LJ59stDRRr9KBGghUbSHYU1s2",
-        "Xoq48VdR7dVFCpnrlS74mDbMcg92",
-        "L8V8XAXYpBP8SyXMAvggC8uWwDz2",
-        "lZwxtl9L7sS5lSOfXrtg3TpXznW2",
-        "F3C2gUQEoJURjIbLAahrWZB7gei1",
-        "oDuDlOgUInQH0DhKkmYe4FLusYn1",
-        "kZe6fi0EYtMLj2t0KKSCjL7Auc52",
-        "Dbr71cK84hTCxk2itmDBWeILPlS2",
-        "svvwd7Nw36R1vIdwhTtvg4Yiikf1",
-        "hk5ZvBR340RCdEAAG5E1Dstav0o1",
-        "pU5TTAMVE5TJt712X1cFpqLP3UD3",
-        "J5z5k20kN2Vi6UjzVYHkfJskLM92",
-        "IIhVf8x1RicwEVdp7TkkH86JBrF3",
-      ].includes(user.uid)
-    : true;
-});
-
-// Show presale dialog after 5 consecutive seconds in home page
-let timeoutDialogPresaleDeadline: ReturnType<typeof setTimeout>[] = [];
-watch(route, (currRoute) => {
-  if (currRoute.name == NamedRoutes.home) {
-    timeoutDialogPresaleDeadline.push(
-      setTimeout(() => {
-        showDialogPresaleDeadline.value = route.name == NamedRoutes.home;
-      }, 5000),
-    );
-  } else if (currRoute.name != NamedRoutes.home) {
-    timeoutDialogPresaleDeadline.forEach((timer) => clearTimeout(timer));
-    timeoutDialogPresaleDeadline = [];
-  }
-});
-watch(showDialogPresaleDeadline, (showingDialog) => {
-  if (!showingDialog) {
-    timeoutDialogPresaleDeadline.forEach((timer) => clearTimeout(timer));
-    timeoutDialogPresaleDeadline = [];
-  }
-});
 
 // Check if any interaction with the app has ever occurred
 let interacted = false;
@@ -378,11 +243,6 @@ onMounted(() => {
   setTimeout(() => {
     isLoading.value = false;
   }, 2000);
-
-  // Show presale deadline dialog
-  setTimeout(() => {
-    showDialogPresaleDeadline.value = true;
-  }, 500);
 });
 
 /**
