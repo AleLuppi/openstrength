a502dae806f5bad6f7e9e23eeb04e11279b38240
AleLuppi <alelup@yahoo.it>
2024-03-12 22:35:54 +0100

    Remove unneded table from program viewer

--- frontend/src/views/ProgramViewerView.vue
+++ frontend/src/views/ProgramViewerView.vue
@@ -33,65 +33,6 @@
         </p>
       </div>
 
-      <!-- Visualized program days -->
-      <div v-if="showTable">
-        <q-table
-          class="q-ma-md q-mb-lg"
-          v-for="(block, indexDay) in programSnapshot?.weekdays"
-          :key="indexDay"
-          :title="`${$t('coach.program_management.builder.week_name', {
-            week: block.weekName,
-          })} - ${$t('coach.program_management.builder.day_name', {
-            day: block.dayName,
-          })}`"
-          :rows="block.exercises"
-          :columns="columns"
-          wrap-cells
-          row-key="name"
-          :pagination="{ rowsPerPage: 0 }"
-          flat
-          bordered
-          hide-bottom
-          separator="cell"
-          dense
-        >
-          <!-- Set header style -->
-          <template v-slot:header="props">
-            <q-tr :props="props" class="bg-table-header">
-              <q-th v-for="col in props.cols" :key="col.name" :props="props">
-                {{ col.label }}
-              </q-th>
-            </q-tr>
-          </template>
-
-          <!-- Custom slot to render values as HTML content -->
-          <template v-slot:body-cell="props">
-            <q-td :props="props">
-              <div class="q-gutter-y-sm">
-                <div
-                  v-for="value in props.value"
-                  :key="props.col + '_' + value"
-                >
-                  {{ value }}
-                </div>
-              </div>
-            </q-td>
-          </template>
-
-          <!-- Custom slot to render exercise title -->
-          <template v-slot:body-cell-exerciseInfo="props">
-            <q-td :props="props">
-              <div class="text-bold">
-                {{ props.value[0] }}
-              </div>
-              <div>
-                {{ props.value[1] }}
-              </div>
-            </q-td>
-          </template>
-        </q-table>
-      </div>
-
       <!-- Show available weeks -->
 
       <!-- Show Workout day -->
@@ -137,10 +78,8 @@
 <script setup lang="ts">
 import { ref, watch, onMounted, computed } from "vue";
 import { useRoute } from "vue-router";
-import { useI18n } from "vue-i18n";
 import { NamedRoutes } from "@/router";
 import { useQuasar } from "quasar";
-import type { QTableProps } from "quasar";
 import { doGetDocs } from "@/helpers/database/readwrite";
 import {
   dbCollections,
@@ -156,9 +95,6 @@ import {
 // Init plugin
 const route = useRoute();
 const $q = useQuasar();
-const i18n = useI18n();
-
-const showTable = ref(false);
 
 // Set card behavior
 const dayDone = ref<boolean[]>([]);
@@ -191,51 +127,6 @@ watch(
     ),
   { immediate: true },
 );
-
-// Set table columns
-const columns: QTableProps["columns"] = [
-  {
-    name: "exerciseInfo",
-    label: i18n.t("coach.program_management.viewer.header_exercise_info"),
-    align: "left",
-    field: (row) => [`${row.exerciseName} ${row.variantName}`, row.note],
-    style: "width: 30%",
-  },
-  {
-    name: "schema",
-    label: i18n.t("coach.program_management.viewer.header_schema"),
-    align: "left",
-    field: "schema",
-    style: "width: 20%",
-  },
-  {
-    name: "schemaNote",
-    label: i18n.t("coach.program_management.viewer.header_schema_note"),
-    align: "left",
-    field: "schemaNote",
-    style: "width: 30%",
-  },
-  {
-    name: "textFeedback",
-    label: i18n.t("coach.program_management.viewer.header_text_feedback"),
-    align: "left",
-    field: (row) =>
-      row.textFeedback.map((val: boolean) =>
-        val ? i18n.t("common.yes") : "-",
-      ),
-    style: "width: 10%",
-  },
-  {
-    name: "videoFeedback",
-    label: i18n.t("coach.program_management.viewer.header_video_feedback"),
-    align: "left",
-    field: (row) =>
-      row.videoFeedback.map((val: boolean) =>
-        val ? i18n.t("common.yes") : "-",
-      ),
-    style: "width: 10%",
-  },
-];
 
 /**
  * Adds the emitted day feedback in the actual athlete feedbacks
