485dd8d8b9c5e29e2e287db6808f482daec5662d
AleLuppi <alelup@yahoo.it>
2024-03-24 17:54:32 +0100

    Clean boot files

--- frontend/src/boot/vueGtag.ts
+++ frontend/src/boot/vueGtag.ts
@@ -1,8 +1,6 @@
 import { boot } from "quasar/wrappers";
 import VueGtag from "vue-gtag";
 
-// "async" is optional;
-// more info on params: https://v2.quasar.dev/quasar-cli/boot-files
 export default boot(async ({ app }) => {
   app.use(VueGtag, {
     config: { id: "G-G8BLW1JL0M" },
