6e6baf968e74e9ee7a8fac09a143c62b8afc9289
AleLuppi <alelup@yahoo.it>
2024-04-04 23:08:23 +0200

    Use consistent week collapse and expand

--- frontend/src/components/ProgramBuilder.vue
+++ frontend/src/components/ProgramBuilder.vue
@@ -37,7 +37,6 @@
         <div
           v-if="index == 0 || week != allWeekDayPairs[index - 1][0]"
           class="row items-center q-gutter-x-xs bg-white q-px-sm q-mx-none q-mb-sm"
-         
         >
           <!-- Week name -->
           <h4
@@ -106,12 +105,7 @@
             flat
             dense
             color="light-dark"
-            @click="
-            () =>
-              allWeekDayPairs.forEach(([weekVal], idx) => {
-                if (weekVal == week) dayInfoCollapsed[idx] = !dayInfoCollapsed[idx];
-              })
-          "
+            @click="toggleWeekCollapse(week)"
           ></q-btn>
         </div>
 
@@ -122,7 +116,6 @@
             v-intersection="dayTitleInteresctionHandler"
             class="row items-center q-gutter-x-xs bg-white q-px-sm q-mx-none q-mb-sm os-day-title"
             :class="{ 'os-day-disabled disabled': !dayCanBeExpanded[index] }"
-          
           >
             <!-- Day name -->
             <h6 class="q-mt-none cursor-pointer text-margin-xs">
@@ -214,11 +207,11 @@
               dense
               color="light-dark"
               @click="
-              () =>
-                (dayInfoCollapsed[index] = dayCanBeExpanded[index]
-                  ? !dayInfoCollapsed[index]
-                  : false)
-            "
+                () =>
+                  (dayInfoCollapsed[index] = dayCanBeExpanded[index]
+                    ? !dayInfoCollapsed[index]
+                    : false)
+              "
             ></q-btn>
           </div>
 
@@ -1046,6 +1039,27 @@ function scrollTo(week: string, day?: string) {
   if (idx < 0) idx = allWeekDayPairs.value.findIndex((val) => val[0] == week);
   if (idx < 0) return;
   exerciseListElement.value?.scrollTo(idx);
+}
+
+/**
+ * Expand or collapse every day in a given week.
+ *
+ * If any day is expanded, collapse all days in the week.
+ * If all days are collapsed, expand them all.
+ *
+ * @param week id of the week that shall be expanded or collapsed.
+ */
+function toggleWeekCollapse(week: string) {
+  let setVal = true;
+  allWeekDayPairs.value
+    .reduce((weekObj: number[], [weekVal], idx) => {
+      if (weekVal == week) {
+        weekObj.push(idx);
+        setVal = setVal && dayInfoCollapsed.value[idx];
+      }
+      return weekObj;
+    }, [])
+    .forEach((idx) => (dayInfoCollapsed.value[idx] = !setVal));
 }
 
 // Set method to handle sticky day title
